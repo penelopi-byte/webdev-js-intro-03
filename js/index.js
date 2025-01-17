9011775bb757e0465f6bfa874977452d61d42fa6
penelopi-byte <Ninnydhynes@gmail.com>
2025-01-16 22:41:07 -0500

    Made text file and answered questions.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,34 @@
 // that are not declared
 "use strict"
 
+/*
+console.log(10 + 20);      // Output: 30
+console.log(10 - 5);       // Output: 5
+console.log(10 * 3);       // Output: 30
+console.log(10 / 2);       // Output: 5
+
+let x = 5;
+x += 10;                  // x = x + 10
+console.log(x);           // Output: 15
+
+console.log(5 == "5");    // Output: true
+console.log(5 === "5");   // Output: false 
+
+console.log(true && false);   // Output: false
+console.log(true || false);   // Output: true
+
+console.log(10 + 5 * 2);       // Output: 20 
+console.log((10 + 5) * 2);     // Output: 30 
+console.log(10 + 5 * 2 - 3);   // Output: 17
+
+
+console.log("5" + 5);          // Output: "55" 
+console.log("5" - 2);          // Output: 3 
+console.log(true + 1);         // Output: 2 
+console.log(false + 1);        // Output: 1 
+
+let a = 10, b = 5;
+console.log(a > b && b < 10);    // Output: true
+console.log(a < b || b > 0);     // Output: true
+console.log(!(a === b));         // Output: true
+*/
